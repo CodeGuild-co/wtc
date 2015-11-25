37746c9d8d73d0d3e0a2c268d100196fd673717e
Will Coates <starrydoesgaming@gmail.com>
2015-11-25 17:10:27 -0500

    Loading posts out of a json document#

--- blog/blog.py
+++ blog/blog.py
@@ -5,10 +5,16 @@ import json
 
 app = Flask(__name__)
 
+def getallposts():
+    data = ''
+    with open('posts.json', 'r') as f:
+        data = f.read()
+    return json.loads(data)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    posts = getallposts()
+    return render_template('home.html', posts=posts.items())
 
 
 @app.route('/posts/<name>/')
@@ -17,12 +23,9 @@ def posts(name):
 
 @app.route('/newposts/<name>/')
 def testpost(name):
-    data = ''
-    with open('posts.json', 'r') as f:
-        data = f.read()
-    realdata = json.loads(data)
+    realdata = getallposts()
     post = realdata[name]
-    return render_template('viewpost.html', title=post['title'], post=post['post'])
+    return render_template('viewpost.html', title=post['title'], post=post['post'], date=post['date'])
 
 if __name__ == '__main__':
     app.secret_key = getenv('SessionKey')
