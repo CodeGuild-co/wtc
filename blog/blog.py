4c3eca6e09dd15bde4b3c0f7b659473eaf3be607
Will Coates <starrydoesgaming@gmail.com>
2015-11-28 12:06:39 +0000

    Login is working

--- blog/blog.py
+++ blog/blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 from os import getenv
 from pymongo import MongoClient
 from datetime import datetime
@@ -9,6 +9,10 @@ db = client.heroku_g8btf552
 posts = db.posts
 
 app = Flask(__name__)
+
+# TODO: Encrypt password
+username = getenv('username', 'test')
+password = getenv('password', 'test')
 
 def getallposts():
     data = ''
@@ -37,15 +41,23 @@ def viewpost(name):
     post  = posts.find_one({'pid': name})
     return render_template('viewpost.html', title=post['title'], post=post['post'], date=datetime.fromtimestamp(post['date']).strftime('%d-%m-%Y'), posts=list(posts.find(sort=[('date', -1)])))
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('viewpost.html', posts=list(posts.find(sort=[('date', -1)])))
+    if request.method == 'POST':
+        un = request.form['username']
+        pw = request.form['password']
+        # TODO: Slow equals
+        if un == username and pw == password:
+            session['isadmin'] = True
+            return redirect('/')
+        return render_template('login.html', un=un, posts=list(posts.find(sort=[('date', -1)])))
+    else:
+        return render_template('login.html', posts=list(posts.find(sort=[('date', -1)])))
 
-@app.route('/login', methods=['POST'])
-def login():
-    # TODO: Add login logic here
-    return render_template('viewpost.html', posts=list(posts.find(sort=[('date', -1)])))
+@app.route('/logout')
+def logout():
+    session['isadmin'] = False
 
 if __name__ == '__main__':
-    app.secret_key = getenv('SessionKey')
+    app.secret_key = getenv('SessionKey') or 'not will coates'
     app.run(debug=True, host='0.0.0.0')
