f46a00779c82bacc1c1e6e1a1902cec295c63ede
William Coates <starrydoesgaming@gmail.com>
2015-12-01 16:08:13 +0000

    Reenabled Facebook

--- blog/chat.py
+++ blog/chat.py
@@ -20,21 +20,21 @@ fb_secret = getenv('FB_SECRET')
 
 @app.route('/chat')
 def chat():
-	#if 'accesskey' not in session:
-	#	if 'error_reason' in request.args:
-	#		return 'You must login via Facebook to use our chat!'
-	#	elif 'code' in request.args:
-	#		resp = ''
-	#		with urlopen('https://graph.facebook.com/v2.3/oauth/access_token?client_id=%s&redirect_uri=http://wtc.codeguild.co/chat&client_secret=%s&code=%s' % (fb_appid, fb_secret, request.args['code'])) as r:
-	#			resp = r.read()
-	#		j = loadjson(resp.decode("utf-8"))
-	#		if 'access_token' in j:
-	#			session['accesskey'] = j['access_token']
-	#			return render_template('chat.html')
-	#		else:
-	#			return 'An error has occured, please try again later'
-	#	else:
-	#		return redirect('https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=http://wtc.codeguild.co/chat&response_type=code' % fb_appid)
+	if 'accesskey' not in session:
+		if 'error_reason' in request.args:
+			return 'You must login via Facebook to use our chat!'
+		elif 'code' in request.args:
+			resp = ''
+			with urlopen('https://graph.facebook.com/v2.3/oauth/access_token?client_id=%s&redirect_uri=http://wtc.codeguild.co/chat&client_secret=%s&code=%s' % (fb_appid, fb_secret, request.args['code'])) as r:
+				resp = r.read()
+			j = loadjson(resp.decode("utf-8"))
+			if 'access_token' in j:
+				session['accesskey'] = j['access_token']
+				return redirect('/chat')
+			else:
+				return 'An error has occured, please try again later'
+		else:
+			return redirect('https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=http://wtc.codeguild.co/chat&response_type=code' % fb_appid)
 	return render_template('chat.html')
 
 @socketio.on('message')
@@ -52,13 +52,13 @@ def handle_message(json):
 
 @socketio.on('connect')
 def connect():
-	#if 'accesskey' not in session:
-	#	return False
-	#resp = ''
-	#with urlopen('https://graph.facebook.com/v2.3/me?client_id=%s&client_secret=%s&accses_token=%s' % (fb_appid, fb_secret, session['accesskey'])) as r:
-	#	resp = r.read()
-	#j = loadjson(resp.decode("utf-8"))
-	session['displayname'] = 'TestUser' #j['name']
+	if 'accesskey' not in session:
+		return False
+	resp = ''
+	with urlopen('https://graph.facebook.com/v2.3/me?client_id=%s&client_secret=%s&accses_token=%s' % (fb_appid, fb_secret, session['accesskey'])) as r:
+		resp = r.read()
+	j = loadjson(resp.decode("utf-8"))
+	session['displayname'] = j['name']
 	emit('message', {
 		'room': 'broadcast',
 		'msg': 'Welcome to Will Coates\' Chat',
